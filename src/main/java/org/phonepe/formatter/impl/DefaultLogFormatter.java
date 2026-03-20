e0cb259d0a867b20563eb3d1ebf8116522ba4561
Ankit Sharma <sharmankit9593@gmail.com>
2026-03-20 23:00:35 +0530

    Added log formatter default implementation and interface

---
+++ src/main/java/org/phonepe/formatter/impl/DefaultLogFormatter.java
@@ -0,0 +1,46 @@
+package org.phonepe.formatter.impl;
+
+import org.phonepe.formatter.LogFormatter;
+import org.phonepe.message.LogMessage;
+
+import java.time.format.DateTimeFormatter;
+
+public class DefaultLogFormatter implements LogFormatter {
+    private final DateTimeFormatter dateTimeFormatter;
+
+    public DefaultLogFormatter(DateTimeFormatter dateTimeFormatter) {
+        this.dateTimeFormatter = dateTimeFormatter;
+    }
+
+    public DefaultLogFormatter() {
+        this(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss,SSS"));
+    }
+
+    @Override
+    public String format(LogMessage logMessage) {
+        if (logMessage == null) {
+            throw new IllegalArgumentException("LogMessage cannot be null");
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(logMessage.getLevel().name());
+
+        sb.append(" [").append(logMessage.getTimestamp().format(dateTimeFormatter)).append("] ");
+
+        if (logMessage.getHostName() != null && !logMessage.getHostName().isBlank()) {
+            sb.append("[").append(logMessage.getHostName()).append("] ");
+        }
+
+        if (logMessage.getTrackingId() != null && !logMessage.getTrackingId().isBlank()) {
+            sb.append("[").append(logMessage.getTrackingId()).append("] ");
+        }
+
+        if (logMessage.getNamespace() != null && !logMessage.getNamespace().isBlank()) {
+            sb.append("[").append(logMessage.getNamespace()).append("] ");
+        }
+
+        sb.append(logMessage.getContent());
+
+        return sb.toString();
+    }
+}
